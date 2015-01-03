228e9c1cd7d6320abe8cfb6367b76612ba09a7e5
Inderpal Singh <inderpal@Admins-MacBook-Pro-5.local>
2015-01-04 04:26:56 +0530

    added sharedpreference util

---
+++ src/main/java/org/porter/utils/PorterShared.java
@@ -0,0 +1,25 @@
+package org.porter.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class PorterShared {
+  static final String PREF_USER_NAME= "username";
+
+  static SharedPreferences getSharedPreferences(Context context) {
+    return PreferenceManager.getDefaultSharedPreferences(context);
+  }
+
+  public static void loginUser(Context context, String userName)
+  {
+    SharedPreferences.Editor editor = getSharedPreferences(context).edit();
+    editor.putString(PREF_USER_NAME, userName);
+    editor.commit();
+  }
+
+  public static String isLoggedIn(Context context)
+  {
+    return getSharedPreferences(context).getString(PREF_USER_NAME, "");
+  }
+}
