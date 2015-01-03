fd4f424b3a4eea29f6947fe92fa9c2164f759e99
Inderpal Singh <inderpal@Admins-MacBook-Pro-5.local>
2015-01-04 02:29:13 +0530

    loading new ride activity after splashscreen

--- src/main/java/org/porter/SplashScreenActivity.java
+++ src/main/java/org/porter/SplashScreenActivity.java
@@ -1,13 +1,33 @@
 package org.porter;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends Activity {
+
+  private Thread splashThread;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_splash_screen);
+
+    splashThread = new Thread() {
+      @Override
+      public void run() {
+        try {
+          synchronized (this) {
+            wait(4000);
+          }
+        } catch (InterruptedException ex) { }
+        Intent intent = new Intent(getApplicationContext(), NewRideActivity.class);
+        startActivity(intent);
+        finish();
+        splashThread.interrupt();
+      }
+    };
+    splashThread.start();
   }
 }
