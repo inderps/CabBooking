878f8cb550c90de5c3b9131e315b1e6f9fabdb21
Inderpal Singh <inderpal@Admins-MacBook-Pro-5.local>
2015-01-04 02:29:46 +0530

    added androidactive framework

---
+++ src/main/java/org/porter/PorterApplication.java
@@ -0,0 +1,13 @@
+package org.porter;
+
+import android.app.Application;
+
+import com.activeandroid.ActiveAndroid;
+
+public class PorterApplication extends Application{
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    ActiveAndroid.initialize(this);
+  }
+}
