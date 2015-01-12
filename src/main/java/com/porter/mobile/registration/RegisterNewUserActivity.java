d1ed70a9d7a193a05d9c02bad794212a10d1efc5
Inderpal Singh <inderpal@Admins-MacBook-Pro-5.local>
2015-01-12 20:55:44 +0530

    login screen design (WIP)

--- src/main/java/com/porter/mobile/registration/RegisterNewUserActivity.java
+++ src/main/java/com/porter/mobile/registration/RegisterNewUserActivity.java
@@ -1,9 +1,13 @@
 package com.porter.mobile.registration;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.porter.mobile.login.LoginActivity;
 
 import org.porter.R;
 
@@ -13,5 +17,10 @@ public class RegisterNewUserActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_register_new_user);
+  }
+
+  public void login(View view) {
+    Intent intent = new Intent(this, LoginActivity.class);
+    startActivity(intent);
   }
 }
