ef146ff1a535076eb566f9b848c8e523bfaba698
Inderpal Singh <inderpal@Admins-MacBook-Pro-5.local>
2015-01-14 23:05:50 +0530

    vehicle selector

--- src/main/java/com/porter/mobile/booking/vehicle/selector/VehicleSelector.java
+++ src/main/java/com/porter/mobile/booking/vehicle/selector/VehicleSelector.java
@@ -8,6 +8,8 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 
+import com.porter.utils.PorterTextView;
+
 import org.porter.R;
 
 import java.util.ArrayList;
@@ -16,5 +18,54 @@ public class VehicleSelector extends RelativeLayout {
   public VehicleSelector(Context context, AttributeSet attrs) {
     super(context, attrs);
     ((Activity)getContext()).getLayoutInflater().inflate(R.layout.vehicle_selector, this, true);
+    setSeekbarAutoMovement();
+    selectVehicle(R.id.tataace_layout, R.id.tataace_label);
+    unSelectVehicle(R.id.tata909_layout, R.id.tata909_label);
+  }
+
+  private void setSeekbarAutoMovement() {
+    SeekBar seekBar = (SeekBar)findViewById(R.id.vehicle_seek_baar);
+    seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if(progress < 50){
+          selectVehicle(R.id.tataace_layout, R.id.tataace_label);
+          unSelectVehicle(R.id.tata909_layout, R.id.tata909_label);
+        }
+        else{
+          selectVehicle(R.id.tata909_layout, R.id.tata909_label);
+          unSelectVehicle(R.id.tataace_layout, R.id.tataace_label);
+        }
+      }
+
+      @Override
+      public void onStartTrackingTouch(SeekBar seekBar) {
+
+      }
+
+      @Override
+      public void onStopTrackingTouch(SeekBar seekBar) {
+        if(seekBar.getProgress() < 50){
+          seekBar.setProgress(0);
+        }
+        else{
+          seekBar.setProgress(100);
+        }
+      }
+    });
+  }
+
+  private void selectVehicle(int vehicleLayoutId, int vehicleLabelId){
+    LinearLayout vehicleLayout = (LinearLayout)findViewById(vehicleLayoutId);
+    vehicleLayout.setBackgroundResource(R.drawable.vehicle_background_selected);
+    PorterTextView vehicleLabel = (PorterTextView)findViewById(vehicleLabelId);
+    vehicleLabel.setTextColor(getResources().getColor(R.color.white));
+  }
+
+  private void unSelectVehicle(int vehicleLayoutId, int vehicleLabelId){
+    LinearLayout vehicleLayout = (LinearLayout)findViewById(vehicleLayoutId);
+    vehicleLayout.setBackgroundResource(R.drawable.vehicle_background);
+    PorterTextView vehicleLabel = (PorterTextView)findViewById(vehicleLabelId);
+    vehicleLabel.setTextColor(getResources().getColor(R.color.blue));
   }
 }
