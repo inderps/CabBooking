1384cf38ec7e21de11ec78ff753c891aa6043fd9
Inderpal Singh <inderpal@Admins-MacBook-Pro-5.local>
2015-01-16 17:01:57 +0530

    wired login with splash screen

--- src/main/java/com/porter/mobile/SplashScreenActivity.java
+++ src/main/java/com/porter/mobile/SplashScreenActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.porter.mobile.login.LoginActivity;
 import com.porter.mobile.navigation.NavigationDrawerActivity;
 
 import org.porter.R;
@@ -22,10 +23,10 @@ public class SplashScreenActivity extends Activity {
       public void run() {
         try {
           synchronized (this) {
-            wait(4000);
+            wait(100);
           }
         } catch (InterruptedException ex) { }
-        Intent intent = new Intent(getApplicationContext(), NavigationDrawerActivity.class);
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
         startActivity(intent);
         finish();
         splashThread.interrupt();
--- src/main/java/com/porter/mobile/login/LoginActivity.java
+++ src/main/java/com/porter/mobile/login/LoginActivity.java
@@ -1,9 +1,14 @@
 package com.porter.mobile.login;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.porter.mobile.navigation.NavigationDrawerActivity;
+import com.porter.mobile.registration.RegisterNewUserActivity;
 
 import org.porter.R;
 
@@ -13,5 +18,15 @@ public class LoginActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_login);
+  }
+
+  public void login(View view) {
+    Intent intent = new Intent(this, NavigationDrawerActivity.class);
+    startActivity(intent);
+  }
+
+  public void signup(View view) {
+    Intent intent = new Intent(this, RegisterNewUserActivity.class);
+    startActivity(intent);
   }
 }
