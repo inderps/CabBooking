c050ab5e805dca73cabfe81f42a7fc6eef52abab
Inderpal Singh <inderpal@Admins-MacBook-Pro-5.local>
2015-01-17 16:49:36 +0530

    new vehicle selector

--- src/main/java/com/porter/mobile/booking/vehicle/selector/VehicleSelector.java
+++ src/main/java/com/porter/mobile/booking/vehicle/selector/VehicleSelector.java
@@ -3,69 +3,38 @@ package com.porter.mobile.booking.vehicle.selector;
 import android.app.Activity;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 
 import com.porter.rest.model.Vehicle;
-import com.porter.utils.PorterTextView;
 
 import org.porter.R;
-
-import java.util.ArrayList;
 
 public class VehicleSelector extends RelativeLayout {
   public VehicleSelector(Context context, AttributeSet attrs) {
     super(context, attrs);
     ((Activity)getContext()).getLayoutInflater().inflate(R.layout.vehicle_selector, this, true);
     setSeekbarAutoMovement();
-    selectVehicle(R.id.tataace_layout, R.id.tataace_label);
-    unSelectVehicle(R.id.tata909_layout, R.id.tata909_label);
-    bindVehicleDetailsPopup();
-  }
-
-  private void bindVehicleDetailsPopup() {
-    LinearLayout tataAce = (LinearLayout)findViewById(R.id.tataace_layout);
-    tataAce.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View v) {
-          selectVehicle(R.id.tataace_layout, R.id.tataace_label);
-          unSelectVehicle(R.id.tata909_layout, R.id.tata909_label);
-          new VehiclePopup(getContext(), new Vehicle());
-      }
-    });
-
-    LinearLayout tata909 = (LinearLayout)findViewById(R.id.tata909_layout);
-    tata909.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        selectVehicle(R.id.tata909_layout, R.id.tata909_label);
-        unSelectVehicle(R.id.tataace_layout, R.id.tataace_label);
-        new VehiclePopup(getContext(), new Vehicle());
-      }
-    });
+    selectVehicle(R.drawable.tata_ace_selected);
   }
 
   private void setSeekbarAutoMovement() {
-    SeekBar seekBar = (SeekBar)findViewById(R.id.vehicle_seek_baar);
+    final SeekBar seekBar = (SeekBar)findViewById(R.id.vehicle_seek_baar);
     seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
       @Override
       public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
         if(progress < 50){
-          selectVehicle(R.id.tataace_layout, R.id.tataace_label);
-          unSelectVehicle(R.id.tata909_layout, R.id.tata909_label);
+          selectVehicle(R.drawable.tata_ace_selected);
         }
         else{
-          selectVehicle(R.id.tata909_layout, R.id.tata909_label);
-          unSelectVehicle(R.id.tataace_layout, R.id.tataace_label);
+          selectVehicle(R.drawable.tata_909_selected);
         }
       }
 
       @Override
       public void onStartTrackingTouch(SeekBar seekBar) {
-
       }
 
       @Override
@@ -78,19 +47,36 @@ public class VehicleSelector extends RelativeLayout {
         }
       }
     });
+
+    seekBar.setOnTouchListener(new OnTouchListener() {
+      @Override
+      public boolean onTouch(View v, MotionEvent event) {
+        boolean changedPosition = false;
+        if(event.getAction() == MotionEvent.ACTION_MOVE)
+        {
+          changedPosition = true;
+          seekBar.setProgress(seekBar.getProgress());
+          return false;
+        }
+        else if (event.getAction() == MotionEvent.ACTION_UP)
+        {
+          if(!changedPosition)
+          {
+            if(seekBar.getProgress() == 0){
+              new VehiclePopup(getContext(), new Vehicle());
+            }
+            else if(seekBar.getProgress() == 100){
+              new VehiclePopup(getContext(), new Vehicle());
+            }
+          }
+        }
+        return false;
+      }
+    });
   }
 
-  private void selectVehicle(int vehicleLayoutId, int vehicleLabelId){
-    LinearLayout vehicleLayout = (LinearLayout)findViewById(vehicleLayoutId);
-    vehicleLayout.setBackgroundResource(R.drawable.vehicle_background_selected);
-    PorterTextView vehicleLabel = (PorterTextView)findViewById(vehicleLabelId);
-    vehicleLabel.setTextColor(getResources().getColor(R.color.white));
-  }
-
-  private void unSelectVehicle(int vehicleLayoutId, int vehicleLabelId){
-    LinearLayout vehicleLayout = (LinearLayout)findViewById(vehicleLayoutId);
-    vehicleLayout.setBackgroundResource(R.drawable.vehicle_background);
-    PorterTextView vehicleLabel = (PorterTextView)findViewById(vehicleLabelId);
-    vehicleLabel.setTextColor(getResources().getColor(R.color.blue));
+  private void selectVehicle(int vehicleId){
+    SeekBar seekBar = (SeekBar)findViewById(R.id.vehicle_seek_baar);
+    seekBar.setThumb(getResources().getDrawable(vehicleId));
   }
 }
